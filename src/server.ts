a15ec80f92c88b364258c8219d0f03b4dda2def3
dhouglas <dhouglasbn@gmail.com>
2026-03-14 22:57:11 -0300

    refactor: removing url from listening log

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,27 @@
 // biome-ignore assist/source/organizeImports: <i dont care>
-import express from "express";
-import cors from "cors";
-import { env } from "./env.ts";
-import { userRoutes } from "./routes/userRoutes.ts";
-import { serviceRoutes } from "./routes/serviceRoutes.ts";
-import { ticketRoutes } from "./routes/ticketRoutes.ts";
-import { swaggerSpec, swaggerUiMiddleware } from "./swagger.ts";
+import express from "express"
+import cors from "cors"
+import { env } from "./env.ts"
+import { userRoutes } from "./routes/userRoutes.ts"
+import { serviceRoutes } from "./routes/serviceRoutes.ts"
+import { ticketRoutes } from "./routes/ticketRoutes.ts"
+import { swaggerSpec, swaggerUiMiddleware } from "./swagger.ts"
 
-const app = express();
-const PORT = env.PORT || 3333;
+const app = express()
+const PORT = env.PORT || 3333
 
-app.use(cors());
-app.use(express.json());
-app.use(
-	"/docs",
-	swaggerUiMiddleware.serve,
-	swaggerUiMiddleware.setup(swaggerSpec),
-);
+app.use(cors())
+app.use(express.json())
+app.use("/docs", swaggerUiMiddleware.serve, swaggerUiMiddleware.setup(swaggerSpec))
 
-app.get("/docs-json", (req, res) => {
-  res.json(swaggerSpec);
-});
+app.get("/docs-json", (_req, res) => {
+	res.json(swaggerSpec)
+})
 
-app.use("/users", userRoutes);
-app.use("/services", serviceRoutes);
-app.use("/tickets", ticketRoutes);
-app.get("/health", (_req, res) => res.send("OK"));
+app.use("/users", userRoutes)
+app.use("/services", serviceRoutes)
+app.use("/tickets", ticketRoutes)
+app.get("/health", (_req, res) => res.send("OK"))
 
 app.listen(
 	{
@@ -33,6 +29,6 @@ app.listen(
 		port: PORT,
 	},
 	() => {
-		console.log(`Server is running on http://localhost:${PORT}`);
+		console.log(`Server is running on port ${PORT} 🚀`)
 	},
-);
+)
